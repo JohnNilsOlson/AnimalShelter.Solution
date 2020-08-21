4a1ad7771e53c23305280f549dc18ce21809bf6b
JohnNils Olson <johnnils@gmail.com>
2020-08-21 09:40:31 -0700

    add id property to properly create db table

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -2,6 +2,7 @@ namespace AnimalShelter.Models
 {
   public class Animal
   {
+    public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Gender { get; set; }
     public int Age { get; set; }
