4c0ba5e5934045e52081f7671771542023773828
JohnNils Olson <johnnils@gmail.com>
2020-08-21 08:56:18 -0700

    add initial database migration

---
+++ AnimalShelter/Migrations/20200821155406_initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Canines",
+                columns: table => new
+                {
+                    CanineId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    Bio = table.Column<string>(nullable: true),
+                    Species = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Canines", x => x.CanineId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Felines",
+                columns: table => new
+                {
+                    FelineId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Weight = table.Column<int>(nullable: false),
+                    Bio = table.Column<string>(nullable: true),
+                    Species = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Felines", x => x.FelineId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Canines");
+
+            migrationBuilder.DropTable(
+                name: "Felines");
+        }
+    }
+}
