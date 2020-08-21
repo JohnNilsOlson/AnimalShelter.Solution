b7b1fc811372dd370ec37e890637632077cb3bf0
JohnNils Olson <johnnils@gmail.com>
2020-08-21 14:55:02 -0700

    add new animal types to database, expand database seeding

---
+++ AnimalShelter/Migrations/20200821215107_expandanimaltypes.cs
@@ -0,0 +1,288 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class expandanimaltypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BirdBreed",
+                table: "Animals",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EquineBreed",
+                table: "Animals",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReptileBreed",
+                table: "Animals",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RodentBreed",
+                table: "Animals",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "BirdBreed" },
+                values: new object[] { 11, 3, "Very communicative!", "Bird", "Male", "Chirpy", 1, "Parakeet" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "ReptileBreed" },
+                values: new object[,]
+                {
+                    { 23, 3, "Loves a good maze!", "Reptile", "Female", "Creampuff", 2, null },
+                    { 22, 1, "Goes west!", "Reptile", "Male", "Fievel", 1, null },
+                    { 21, 2, "Loves cheese!", "Reptile", "Male", "Templeton", 1, null },
+                    { 20, 2, "Has a British Accent!", "Reptile", "Male", "Geico", 1, "Gecko" },
+                    { 19, 8, "Loves to wrestle!", "Reptile", "Female", "Alli", 46, "Alligator" },
+                    { 18, 5, "Loves lounging in the sun!", "Reptile", "Male", "Puff", 24, "Kimodo Dragon" },
+                    { 17, 13, "Gives great hugs!", "Reptile", "Female", "Kaa", 47, "Boa Constrictor" },
+                    { 16, 4, "Will bite!", "Reptile", "Female", "Chompy", 1, "Snapping Turtle" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "FelineBreed" },
+                values: new object[,]
+                {
+                    { 10, 8, "Hypo-allergenic!", "Feline", "Female", "Fluffy", 9, "Sphynx" },
+                    { 9, 4, "Dumb but loveable!", "Feline", "Male", "Stimpy", 15, "Manx" },
+                    { 8, 9, "Loves chasing toys!", "Feline", "Female", "Snowball", 2, "Persian" },
+                    { 7, 6, "Ornery!", "Feline", "Male", "Simba", 12, "Maine Coon" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "ReptileBreed" },
+                values: new object[] { 24, 4, "Cuddly!", "Reptile", "Female", "Cookie", 3, null });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "FelineBreed" },
+                values: new object[] { 6, 3, "Loves belly rubs!", "Feline", "Male", "Whiskers", 10, "Tabby" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "EquineBreed" },
+                values: new object[,]
+                {
+                    { 29, 7, "Can do math!", "Equine", "Female", "Einstein", 564, "Miniature" },
+                    { 28, 5, "Kind hearted!", "Equine", "Male", "Black Beauty", 1124, "American Quarter" },
+                    { 27, 12, "Very fast!", "Equine", "Male", "Secretariat", 986, "Thoroughbred" },
+                    { 26, 6, "Adventurous!", "Equine", "Male", "Artax", 1256, "Arabian" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "CanineBreed" },
+                values: new object[,]
+                {
+                    { 5, 5, "Courageous!", "Canine", "Female", "Spot", 41, "Dalmation" },
+                    { 4, 3, "Very loyal!", "Canine", "Male", "Lassie", 47, "Border Collie" },
+                    { 3, 6, "Yappy!", "Canine", "Male", "Ren", 12, "Chihuahua" },
+                    { 2, 2, "Needs leash traning!", "Canine", "Female", "Ramona", 55, "German Shepard" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "BirdBreed" },
+                values: new object[,]
+                {
+                    { 15, 14, "Extremely fast!", "Bird", "Female", "Ozzie", 45, "Emu" },
+                    { 14, 5, "Regal!", "Bird", "Female", "Sam", 12, "Bald Eagle" },
+                    { 13, 9, "Not friendly!", "Bird", "Male", "Blue", 3, "Macaw" },
+                    { 12, 12, "Escape artist!", "Bird", "Female", "Stripes", 1, "Zebra Finch" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "EquineBreed" },
+                values: new object[] { 30, 9, "A former movie star!", "Equine", "Male", "Cojo Rojo", 1347, "Appaloosa" });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "ReptileBreed" },
+                values: new object[] { 25, 2, "Loves to play hide and seek!", "Reptile", "Male", "Slinky", 3, null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 25);
+
+            migrationBuilder.DropColumn(
+                name: "BirdBreed",
+                table: "Animals");
+
+            migrationBuilder.DropColumn(
+                name: "EquineBreed",
+                table: "Animals");
+
+            migrationBuilder.DropColumn(
+                name: "ReptileBreed",
+                table: "Animals");
+
+            migrationBuilder.DropColumn(
+                name: "RodentBreed",
+                table: "Animals");
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Bio", "Discriminator", "Gender", "Name", "Weight", "FelineBreed" },
+                values: new object[] { 2, 3, "Loves belly rubs!", "Feline", "Male", "Whiskers", 10, "Tabby" });
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -8,12 +8,52 @@ namespace AnimalShelter.Models
     {
       builder.Entity<Canine>()
         .HasData(
-          new Canine { AnimalId = 1, CanineBreed = "Labrador Retreiver", Name = "Rex", Age = 7, Gender = "Female", Weight = 42, Bio = "Loves fetch!" }
+          new Canine { AnimalId = 1, CanineBreed = "Labrador Retreiver", Name = "Rex", Age = 7, Gender = "Female", Weight = 42, Bio = "Loves fetch!" },
+          new Canine { AnimalId = 2, CanineBreed = "German Shepard", Name = "Ramona", Age = 2, Gender = "Female", Weight = 55, Bio = "Needs leash traning!" },
+          new Canine { AnimalId = 3, CanineBreed = "Chihuahua", Name = "Ren", Age = 6, Gender = "Male", Weight = 12, Bio = "Yappy!" },
+          new Canine { AnimalId = 4, CanineBreed = "Border Collie", Name = "Lassie", Age = 3, Gender = "Male", Weight = 47, Bio = "Very loyal!" },
+          new Canine { AnimalId = 5, CanineBreed = "Dalmation", Name = "Spot", Age = 5, Gender = "Female", Weight = 41, Bio = "Courageous!" }
         );
       builder.Entity<Feline>()
         .HasData(
-          new Feline { AnimalId = 2, FelineBreed = "Tabby", Name = "Whiskers", Age = 3, Gender = "Male", Weight = 10, Bio = "Loves belly rubs!" }
+          new Feline { AnimalId = 6, FelineBreed = "Tabby", Name = "Whiskers", Age = 3, Gender = "Male", Weight = 10, Bio = "Loves belly rubs!" },
+          new Feline { AnimalId = 7, FelineBreed = "Maine Coon", Name = "Simba", Age = 6, Gender = "Male", Weight = 12, Bio = "Ornery!" },
+          new Feline { AnimalId = 8, FelineBreed = "Persian", Name = "Snowball", Age = 9, Gender = "Female", Weight = 2, Bio = "Loves chasing toys!" },
+          new Feline { AnimalId = 9, FelineBreed = "Manx", Name = "Stimpy", Age = 4, Gender = "Male", Weight = 15, Bio = "Dumb but loveable!" },
+          new Feline { AnimalId = 10, FelineBreed = "Sphynx", Name = "Fluffy", Age = 8, Gender = "Female", Weight = 9, Bio = "Hypo-allergenic!" }
         );
+      builder.Entity<Bird>()
+        .HasData(
+          new Bird { AnimalId = 11, BirdBreed = "Parakeet", Name = "Chirpy", Age = 3, Gender = "Male", Weight = 1, Bio = "Very communicative!" },
+          new Bird { AnimalId = 12, BirdBreed = "Zebra Finch", Name = "Stripes", Age = 12, Gender = "Female", Weight = 1, Bio = "Escape artist!" },
+          new Bird { AnimalId = 13, BirdBreed = "Macaw", Name = "Blue", Age = 9, Gender = "Male", Weight = 3, Bio = "Not friendly!" },
+          new Bird { AnimalId = 14, BirdBreed = "Bald Eagle", Name = "Sam", Age = 5, Gender = "Female", Weight = 12, Bio = "Regal!" },
+          new Bird { AnimalId = 15, BirdBreed = "Emu", Name = "Ozzie", Age = 14, Gender = "Female", Weight = 45, Bio = "Extremely fast!" }
+        );
+      builder.Entity<Reptile>()
+      .HasData(
+        new Reptile { AnimalId = 16, ReptileBreed = "Snapping Turtle", Name = "Chompy", Age = 4, Gender = "Female", Weight = 1, Bio = "Will bite!" },
+        new Reptile { AnimalId = 17, ReptileBreed = "Boa Constrictor", Name = "Kaa", Age = 13, Gender = "Female", Weight = 47, Bio = "Gives great hugs!" },
+        new Reptile { AnimalId = 18, ReptileBreed = "Kimodo Dragon", Name = "Puff", Age = 5, Gender = "Male", Weight = 24, Bio = "Loves lounging in the sun!" },
+        new Reptile { AnimalId = 19, ReptileBreed = "Alligator", Name = "Alli", Age = 8, Gender = "Female", Weight = 46, Bio = "Loves to wrestle!" },
+        new Reptile { AnimalId = 20, ReptileBreed = "Gecko", Name = "Geico", Age = 2, Gender = "Male", Weight = 1, Bio = "Has a British Accent!" }
+      );
+      builder.Entity<Reptile>()
+      .HasData(
+        new Rodent { AnimalId = 21, RodentBreed = "Rat", Name = "Templeton", Age = 2, Gender = "Male", Weight = 1, Bio = "Loves cheese!" },
+        new Rodent { AnimalId = 22, RodentBreed = "Mouse", Name = "Fievel", Age = 1, Gender = "Male", Weight = 1, Bio = "Goes west!" },
+        new Rodent { AnimalId = 23, RodentBreed = "Hamster", Name = "Creampuff", Age = 3, Gender = "Female", Weight = 2, Bio = "Loves a good maze!" },
+        new Rodent { AnimalId = 24, RodentBreed = "Guinea Pig", Name = "Cookie", Age = 4, Gender = "Female", Weight = 3, Bio = "Cuddly!" },
+        new Rodent { AnimalId = 25, RodentBreed = "Weasel", Name = "Slinky", Age = 2, Gender = "Male", Weight = 3, Bio = "Loves to play hide and seek!" }
+      );
+      builder.Entity<Equine>()
+      .HasData(
+        new Equine { AnimalId = 26, EquineBreed = "Arabian", Name = "Artax", Age = 6, Gender = "Male", Weight = 1256, Bio = "Adventurous!" },
+        new Equine { AnimalId = 27, EquineBreed = "Thoroughbred", Name = "Secretariat", Age = 12, Gender = "Male", Weight = 986, Bio = "Very fast!" },
+        new Equine { AnimalId = 28, EquineBreed = "American Quarter", Name = "Black Beauty", Age = 5, Gender = "Male", Weight = 1124, Bio = "Kind hearted!" },
+        new Equine { AnimalId = 29, EquineBreed = "Miniature", Name = "Einstein", Age = 7, Gender = "Female", Weight = 564, Bio = "Can do math!" },
+        new Equine { AnimalId = 30, EquineBreed = "Appaloosa", Name = "Cojo Rojo", Age = 9, Gender = "Male", Weight = 1347, Bio = "A former movie star!" }
+      );
     }
     public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
             : base(options) {  }
